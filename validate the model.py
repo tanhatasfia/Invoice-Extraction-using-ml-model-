5f92cbac402dc0c5f9c2ba80266f00ce5f4f0d3b
tanhatasfia <66655911+tanhatasfia@users.noreply.github.com>
2024-09-27 14:55:31 +0600

    Create validate the model.py

---
+++ validate the model.py
@@ -0,0 +1,4 @@
+!python /content/models/research/object_detection/model_main_tf2.py \
+    --pipeline_config_path={pipeline_config_path} \
+    --model_dir={model_dir} \
+    --checkpoint_dir={model_dir}
